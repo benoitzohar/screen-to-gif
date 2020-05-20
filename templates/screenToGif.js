022a6f2c361e52e525d0b30807a11b7d449b6d50
Benoit Zohar <benoit.zohar@gmail.com>
2020-05-20 14:45:20 +0000

    reduce framerate to handle longer videos properly

--- templates/screenToGif.js
+++ templates/screenToGif.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
+const path = require("path");
 const exec = require("child_process").exec;
 
 const DIR = "{PATH_TO_SCREENSHOTS}/";
 const TMP_DIR = "/tmp/screenToGif/";
 
+const deleteDirectoryRecursive = function(dir) {
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach((file, index) => {
+      const curDirc = path.join(dir, file);
+      if (fs.lstatSync(curDirc).isDirectory()) {
+        deleteFolderRecursive(curDirc);
+      } else {
+        fs.unlinkSync(curDirc);
+      }
+    });
+    fs.rmdirSync(dir);
+  }
+};
+
 if (!fs.existsSync(TMP_DIR)) {
   fs.mkdirSync(TMP_DIR);
 }
+else {
+  deleteDirectoryRecursive(TMP_DIR);
+  fs.mkdirSync(TMP_DIR);
+}
+
 
 async function sh(cmd) {
   return new Promise(function (resolve, reject) {
@@ -35,33 +55,35 @@ const getLastScreenRecorderFile = () => {
 const movie = getLastScreenRecorderFile();
 const gifName = movie.replace(".mov", ".gif");
 if (!fs.existsSync(DIR + gifName)) {
-  const extractFrames = `/usr/local/bin/ffmpeg -i "${DIR + movie}" "${
+  const extractFrames = `/usr/local/bin/ffmpeg -i "${DIR + movie}" -r 5 "${
     TMP_DIR + movie
   }.frame%04d.png"`;
-  console.log("Extract frames with: " + extractFrames);
+  console.log("[ScreenToGif] Extract frames with: " + extractFrames);
   sh(extractFrames)
     .then(({ stdout, stderr }) =>
-      console.log("Extracted! " + stdout + "(error:" + stderr + ")")
+      console.log("[ScreenToGif] Extracted! " + stdout + "(error:" + stderr + ")")
     )
     .then(() => {
-      const turnToGif = `/usr/local/bin/gifski --quality 12 --width 800 -o "${DIR + gifName}" ${
+      const frames = fs.readdirSync(TMP_DIR);
+      console.log("[ScreenToGif] Got "+frames.length+" frames...");
+      const turnToGif = `/usr/local/bin/gifski --quality 8 --width 800 --fps 7 -o "${DIR + gifName}" ${
         TMP_DIR + movie.split(" ").join("\\ ")
-      }.frame*.png `;
-      console.log("Turn into gif with: " + turnToGif);
+      }.frame*.png`;
+      console.log("[ScreenToGif] Turn into gif with: " + turnToGif);
 
       return sh(turnToGif).then(({ stdout, stderr }) =>
-        console.log("Gif created! " + stdout + "(error:" + stderr + ")")
+        console.log("[ScreenToGif] Gif created! " + stdout + "(error:" + stderr + ")")
       );
     })
     .then(() => {
-      console.log("Cleaning up frames...");
       const frames = fs.readdirSync(TMP_DIR);
+      console.log("[ScreenToGif] Cleaning up "+frames.length+" frames...");
       frames.forEach(f => fs.unlinkSync(TMP_DIR + f));
     })
     .then(() => {
-      console.log("All done ✅");
+      console.log("[ScreenToGif] All done ✅");
     })
     .catch(console.log);
 } else {
-  console.log("Gif already exists:" + gifName);
+  console.log("[ScreenToGif] Gif already exists:" + gifName);
 }
